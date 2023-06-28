1d228db1a61a0e819d577ad5199c75d53a35383c
KrzyKerlin <krzysztof.kerlin@op.pl>
2023-06-28 08:14:29 +0200

    Fixed score and timer after category selection

--- quiz.js
+++ quiz.js
@@ -33,7 +33,7 @@ function selectCategories() {
 function setStartBtnState() {
   let categorySelected = false;
   categories.forEach((category) => {
-    if (category.classList.contains("selected")) {
+    if (category.classList.contains("catSelected")) {
         categorySelected = true;
         startQuizButton.style.backgroundColor = "#043004";
         startQuizButton.style.letterSpacing = ".2em";
@@ -56,10 +56,23 @@ setStartBtnState();
 //start quiz//
 startQuizButton.addEventListener('click', startQuiz)
 
-function startQuiz() { 
+function startQuiz() {
     categoriesPage.style.display = "none";
     quizPage.style.display = "flex";
-    runQuestion();
+    if (worldCupBtn.classList.contains("catSelected")) {
+        questions = worldCupQuestions;
+        categoryTitle.textContent = "Mistrzostwa Świata";
+    } else if (clubCupsBtn.classList.contains("catSelected")) {
+        questions = clubCupsQuestions;
+        categoryTitle.textContent = "Klubowe Puchary";
+    } else if (leaguesBtn.classList.contains("catSelected")) {
+        questions = leaguesQuestions;
+        categoryTitle.textContent = "Ligi Światowe";
+    } else if (recordsBtn.classList.contains("catSelected")) {
+        questions = recordsQuestions;
+        categoryTitle.textContent = "Rekordy I Ciekawostki";
+    }
+    runQuestion(questions);
     startAndResetTimer();
 }
 
@@ -90,41 +103,80 @@ function startAndResetTimer() {
   }, 1000);  // 1000 ms = 1 sec
 }
 
-//questions and answers//
-let questions = [{
-        question: "Kto został królem strzelców na mundialu we Francji w 1998 roku?",
-        answers: ["Davor Šuker", "Dennis Bergkamp", "Ronaldo Nazario", "Thierry Henry"],
-        correct: answerA,
-    },
-    {
-        question: "Kto zagrał w finale mundialu w 2006 roku?",
-        answers: ["Francja-Brazylia", "Włochy-Francja", "Brazylia-Niemcy", "Hiszpania-Holandia"],
-        correct: answerB,
-    },
+// categories
+const worldCupBtn = document.querySelector("#worldCup");
+const clubCupsBtn = document.querySelector("#clubCups");
+const leaguesBtn = document.querySelector("#leagues");
+const recordsBtn = document.querySelector("#records");
+const categoryTitle = document.querySelector('#category');
+
+// //questions and answers//
+const worldCupQuestions = [
+  {
+    question: "Kto został królem strzelców na mundialu we Francji w 1998 roku?",
+    answers: ["Davor Šuker", "Dennis Bergkamp", "Ronaldo Nazario", "Thierry Henry"],
+    correct: "answerA",
+  },
+  {
+    question: "Kto zagrał w finale mundialu w 2006 roku?",
+    answers: ["Francja-Brazylia", "Włochy-Francja", "Brazylia-Niemcy", "Hiszpania-Holandia"],
+    correct: "answerB",
+  },
+  {
+    question: "Kto jest 2-krotnym Mistrzem Świata?",
+    answers: ["Romario", "Cafu", "Rivaldo", "Roberto Carlos"],
+    correct: answerB,
+},
+{
+    question: "Która reprezentacja zagrała w meczu finałowym mundialu?",
+    answers: ["Portugalia", "Bułgaria", "Szwecja", "Jugosławia"],
+    correct: answerC,
+},
+{
+    question: "Kto jest rekordzistą pod względem liczby zdobytych bramek na mundialu?",
+    answers: ["Ronaldo Nazario", "Miroslav Klose", "Cristiano Ronaldo", "Pelé"],
+    correct: "answerB",
+  },
+  {
+    question: "Kto strzelił decydującego karnego w finale mundialu 2022 roku?",
+    answers: ["Lionel Messi", "Nicolas Otamendi", "Gonzalo Montiel" , "Marcos Acuña"],
+    correct: answerC,
+  }
+];
+
+const clubCupsQuestions = [
     {
         question: "Kto wygrał ostatni Puchar Zdobywcow Pucharów?",
         answers: ["Parma", "Olympique Marseille", "Feyenoord", "Lazio"],
         correct: answerD,
     },
     {
-        question: "Który trener zdobył UEFA Ligę Mistrzów i Mistrzostwo Świata?",
-        answers: ["Vicente del Bosque", "Carlo Ancelotti", "Luiz Felipe Scolari", "Didier Deschamps"],
+        question: "Który klub nie posiada honorowej odznaki Ligi Mistrzów?",
+        answers: ["AC Milan", "FC Barcelona", "Manchester United", "Ajax Amsterdam"],
+        correct: answerC,
+    },
+    {
+        question: "Kto zagrał w pierwszym finale Pucharu Europy w 1956 roku?",
+        answers: ["Real Madryt - AC Milan", "Real Madryt - Eintracht Frankfurt", "Real Madryt - Stade de Reims", "Real Madryt - Hamburger SV"],
+        correct: answerC,
+    },
+    {
+        question: "Który klub zdobył azjatycką Ligę Mistzrów w 2023 roku?",
+        answers: ["Urawa Red Diamonds", "Al-Hilal", "Gamba Osaka", "FC Seoul"],
+        corerct: answerA,
+    }
+];
+
+const leaguesQuestions = [
+    {
+        question:  "W których sezonach Napoli zdobylo Mistrzostwo Włoch?",
+        answers: ["1986/87, 1989/90, 2022/23", "1988/89, 1989/90, 2022/23", "1980/81, 1986/87, 2022/23", "1982/83, 1986/87, 2022/23"],
         correct: answerA,
     },
     {
         question: "W którym klubie nie grał Christian Vieri?",
         answers: ["Roma", "Milan", "Inter", "Juventus"],
         correct: answerA,
-    },
-    {
-        question: "Kto jest 2-krotnym Mistrzem Świata?",
-        answers: ["Romario", "Cafu", "Rivaldo", "Roberto Carlos"],
-        correct: answerB,
-    },
-    {
-        question: "Który klub nie posiada honorowej odznaki Ligi Mistrzów?",
-        answers: ["AC Milan", "FC Barcelona", "Manchester United", "Ajax Amsterdam"],
-        correct: answerC,
     },
     {
         question: "Która z drużyn nigdy nie została Mistrzem Niemiec?",
@@ -137,21 +189,6 @@ let questions = [{
         correct: answerD,
     },
     {
-        question: "Po kim Ronaldinho przejął numer 10 w PSG?",
-        answers: ["Mikeal Arteta", "Pedro Pauleta", "Nicolas Anelka", "Jay-Jay Okocha"],
-        correct: answerD,
-    },
-    {
-        question: "Która reprezentacja zagrała w meczu finałowym mundialu?",
-        answers: ["Portugalia", "Bułgaria", "Szwecja", "Jugosławia"],
-        correct: answerC,
-    },
-    {
-        question: "W którym roku Holandia zdobyła Mistrzostwo Europy?",
-        answers: ["1992", "1988", "1996", "1980"],
-        correct: answerB,
-    },
-    {
         question: "Który klub ma najwięcej tytułów Mistrza Brazylii?",
         answers: ["SE Palmeiras", "Fluminense FC", "SC Corinthians", "Vasco da Gama"],
         correct: answerA,
@@ -162,19 +199,42 @@ let questions = [{
         correct: answerB,
     },
     {
-        question: "Który piłkarz nie zdobył Złotej Piłki?",
-        answers: ["Andriy Shevchenko", "Micheal Owen", "Steven Gerrard", "Zinédine Zidane"],
-        correct: answerC,
-    },
-    {
         question: "Kogo Roman Abramowicz ściągnął do Chelsea najpóźniej?",
         answers: ["Joe Cole", "Juan Sebastian Veron", "Didier Drogba", "Claude Makalele"],
         correct: answerC,
     },
     {
+        question: "Eredivisie to rozgrywki ligowe w ...",
+        answers: ["Belgii", "Norwegii", "Finlandii", "Holandii"],
+        corerct: answerD,
+    },
+    {
         question: "Z którego kraju pochodzi klub Vissel Kobe?",
         answers: ["Korei Południowej", "Wietnamu", "Chin", "Japonii"],
         correct: answerD,
+    },
+];
+
+const recordsQuestions = [
+    {
+        question: "Który trener zdobył UEFA Ligę Mistrzów i Mistrzostwo Świata?",
+        answers: ["Vicente del Bosque", "Carlo Ancelotti", "Luiz Felipe Scolari", "Didier Deschamps"],
+        correct: answerA,
+    },
+    {
+        question: "W którym roku Holandia zdobyła Mistrzostwo Europy?",
+        answers: ["1992", "1988", "1996", "1980"],
+        correct: answerB,
+    },
+    {
+        question: "Który piłkarz nie zdobył Złotej Piłki?",
+        answers: ["Andriy Shevchenko", "Micheal Owen", "Steven Gerrard", "Zinédine Zidane"],
+        correct: answerC,
+    },
+    {
+        question: "Kto nie był współgospodarzem EURO 2021?",
+        answers: ["Szkocja", "Belgia", "Rosja", "Dania"],
+        correct: answerB,
     },
     {
         question: "Kto ma najwięcej występów w koszulce Realu Madryt?",
@@ -182,18 +242,42 @@ let questions = [{
         correct: answerA,
     },
     {
-        question: "Kto nie będzie współgospodarzem EURO 2021?",
-        answers: ["Szkocja", "Belgia", "Rosja", "Dania"],
-        correct: answerB,
-    },
-    {
         question: "Które drużyny 3-krotnie zdobyły Mistrzostwo Europy?",
         answers: ["Niemcy i Hiszpania", "Niemcy i Włochy", "Francja i Włochy", "Francja i Hiszpania"],
         correct: answerA,
     },
-]
+    {
+        question: "Po kim Ronaldinho przejął numer 10 w PSG?",
+        answers: ["Mikeal Arteta", "Pedro Pauleta", "Nicolas Anelka", "Jay-Jay Okocha"],
+        correct: answerD,
+    },
+];
 
-function runQuestion() {
+let questions = [];
+
+// Obsługa kliknięć na przyciskach kategorii
+worldCupBtn.addEventListener("click", function() {
+    categoryTitle.textContent = "Mistrzostwa Świata"; // zmienia tytuł kategorii
+    runQuestion(worldCupQuestions); // wyświetla pytanie
+});
+    
+clubCupsBtn.addEventListener("click", function() {
+    categoryTitle.textContent = "Klubowe Puchary";
+    runQuestion(clubCupsQuestions);
+});
+    
+leaguesBtn.addEventListener("click", function() {
+    categoryTitle.textContent = "Ligi Światowe";
+    runQuestion(leaguesQuestions);
+});
+    
+recordsBtn.addEventListener("click", function() {
+    categoryTitle.textContent = "Rekordy I Ciekawostki";
+    runQuestion(recordsQuestions);
+});
+    
+    // Funkcja wyświetlająca pytania
+function runQuestion(questions) {
     let quest = questions[questionsIndex];
     questionNumber.textContent = indexNumber + 1 + " / " + questions.length;
     questionTag.textContent = quest.question;
@@ -202,6 +286,17 @@ function runQuestion() {
     answerCTag.innerHTML = "<h4>C.</h4>" + " " + quest.answers[2];
     answerDTag.innerHTML = "<h4>D.</h4>" + " " + quest.answers[3];
 }
+         
+/*
+function runQuestion() {
+    let quest = questions[questionsIndex];
+    questionNumber.textContent = indexNumber + 1 + " / " + questions.length;
+    questionTag.textContent = quest.question;
+    answerATag.innerHTML = "<h4>A.</h4>" + " " + quest.answers[0];
+    answerBTag.innerHTML = "<h4>B.</h4>" + " " + quest.answers[1];
+    answerCTag.innerHTML = "<h4>C.</h4>" + " " + quest.answers[2];
+    answerDTag.innerHTML = "<h4>D.</h4>" + " " + quest.answers[3];
+}*/
 
 const lastQuestion = questions.length - 1;
 
@@ -215,7 +310,7 @@ function setNext() {
 
     if (questionsIndex < lastQuestion) {
         questionsIndex++;
-        runQuestion();
+        runQuestion(questions);
     } else {
         final();
 
